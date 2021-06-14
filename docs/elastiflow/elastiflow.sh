34de77bff11d057b817b7e992ac514bdd5262799
mbelloiseau <mathieu.belloiseau@protonmail.com>
2021-06-14 12:14:27 +0200

    elastiflow : fix copy

--- docs/elastiflow/elastiflow.sh
+++ docs/elastiflow/elastiflow.sh
@@ -7,6 +7,8 @@ python3 -m pip install ansible
 
 ansible --version
 
+test -f elastiflow.yml && rm $_
+
 curl https://raw.githubusercontent.com/simplonco/tssr-cyber-doc/elastiflow_ansible/docs/elastiflow/ansible/main.yml -o elastiflow.yml
 
 ansible-playbook elastiflow.yml
